c2d032c4bb8d0ceb137c2b7f4965ec03eaa0e73c
santech712 <bluestar929@outlook.com>
2022-08-12 03:53:51 +0800

    add loading skeleton

---
+++ src/pages/search/Villas/LoadingItem/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./LoadingItem.module.scss";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const LoadingItem = () => {
+    return (
+        <div className={styles.wrapper}>
+            <Skeleton
+                className={styles.loading}
+                duration={2}
+                width={260}
+                height={160}
+            />
+
+            <div className={styles.content}>
+                <Skeleton
+                    className={styles.loading}
+                    duration={2}
+                    width={360}
+                    height={24}
+                />
+                <Skeleton
+                    className={styles.loading}
+                    duration={2}
+                    width={200}
+                    height={21}
+                />
+                <Skeleton
+                    className={styles.loading}
+                    duration={2}
+                    width={200}
+                    height={21}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default LoadingItem;
--- src/pages/search/Villas/VillaList/index.tsx
+++ src/pages/search/Villas/VillaList/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootReducerType } from "../../../../store";
+import LoadingItem from "../LoadingItem";
 import VillaItem from "../VillaItem";
 import styles from "./VillaList.module.scss";
 
@@ -11,13 +12,11 @@ const VillaList = () => {
 
     return (
         <div className={styles.villaList}>
-            {!loading ? (
-                (data || []).map((villa) => (
-                    <VillaItem key={villa.id} {...villa} />
-                ))
-            ) : (
-                <div></div>
-            )}
+            {!loading
+                ? (data || []).map((villa) => (
+                      <VillaItem key={villa.id} {...villa} />
+                  ))
+                : [0, 1, 2, 3, 4, 5].map((i) => <LoadingItem key={i} />)}
         </div>
     );
 };
