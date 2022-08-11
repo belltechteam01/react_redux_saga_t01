f3d6972fc4a0484797cb4cb0ae72b5045e4c66e7
santech712 <bluestar929@outlook.com>
2022-08-12 03:19:41 +0800

    finish villa list & item

--- src/pages/search/Villas/VillaItem/index.tsx
+++ src/pages/search/Villas/VillaItem/index.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { IVillaType } from "../../../../store/search_module/types";
+import styles from "./VillaItem.module.scss";
+import addressMarkIcon from "../../../../assets/icons/addressMark.svg";
 
-const VillaItem = () => {
-    return <div>villa Item</div>;
+const VillaItem = (props: IVillaType) => {
+    const { img, title, address, slappkamers, badkamers, price } = props;
+
+    return (
+        <div className={styles.itemWrapper}>
+            <div className={styles.itemImage}>
+                <img src={img} alt={img} />
+            </div>
+            <div className={styles.content}>
+                <div className={styles.title}>{title}</div>
+                <div className={styles.address}>
+                    <img src={addressMarkIcon} alt="addrssIcon" />
+                    <span>{address}</span>
+                </div>
+                <div className={styles.details}>
+                    {slappkamers} Slappkamers<span>•</span>
+                    {badkamers} Badkamers
+                </div>
+                <div className={styles.price}>€ {price.toFixed(3)}</div>
+            </div>
+        </div>
+    );
 };
 
 export default VillaItem;
--- src/pages/search/Villas/VillaList/index.tsx
+++ src/pages/search/Villas/VillaList/index.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootReducerType } from "../../../../store";
+import VillaItem from "../VillaItem";
+import styles from "./VillaList.module.scss";
 
 const VillaList = () => {
-    return <div>hello villalist</div>;
+    const state = useSelector((state: RootReducerType) => state.search_villas);
+
+    const { loading, data } = state;
+
+    return (
+        <div className={styles.villaList}>
+            {!loading ? (
+                (data || []).map((villa) => (
+                    <VillaItem key={villa.id} {...villa} />
+                ))
+            ) : (
+                <div></div>
+            )}
+        </div>
+    );
 };
 
 export default VillaList;
