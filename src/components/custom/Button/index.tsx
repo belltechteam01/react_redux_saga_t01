0928dc99455378112adf74a2808ca12c4fd71a12
belltechteam01 <mayun.belltech@outlook.com>
2022-08-20 16:23:12 -0700

    [+] custom component

---
+++ src/components/custom/Button/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import styles from './styles.module.scss';
+
+interface IComponentProps {
+    
+}
+
+export default function Button(props: IComponentProps) {
+    return (
+        <div className={styles.root}>
+            
+        </div>
+    )
+}
+
